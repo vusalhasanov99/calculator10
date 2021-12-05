254a04ea7d2bfb58caeaf8da996eba31a635eced
vusqa12 <you@vusqa1999@gmail.com>
2021-12-05 19:28:07 +0400

    calculator1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace calculatar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Toplama emelyati ucun + dail edin");
+            Console.WriteLine("cixma emelyati ucun - dail edin");
+            Console.WriteLine("vurma emelyati ucun * dail edin");
+            Console.WriteLine("bolme emelyati ucun / dail edin");
+
+            if (Console.ReadLine() == "+")
+            {
+                Console.Write("a-ni daxil et : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b-ni daxil et: ");
+            int b= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("cavab:  ");
+            Console.WriteLine( a + b );
+
+            }
+
+            if  (Console.ReadLine() == "a")
+            {
+                Console.Write("a-ni daxil et : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b-ni daxil et: ");
+            int b= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("cavab: ");
+            Console.WriteLine( a - b );
+
+            }
+
+            
+           
+
+
+
+        }
+    }
+}
